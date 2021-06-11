6cda83646ecc18b80b12079d9ef9fad807ec0e99
juanFdS <juan9794@gmail.com>
2021-06-10 23:28:40 -0300

    Posible resolucion del parcial

--- src/Library.hs
+++ src/Library.hs
@@ -1,2 +1,238 @@
 module Library where
 import PdePreludat
+
+data Tripulante = Tripulante { energia :: Number } deriving (Show, Eq)
+
+type ActividadDiaria = Tripulante -> Tripulante
+
+estaEnLasUltimas :: Tripulante -> Bool
+estaEnLasUltimas = (<50) . energia
+
+aumentarEnergia :: Number -> ActividadDiaria
+aumentarEnergia cantidad (Tripulante n) = Tripulante (n + cantidad)
+
+disminuirEnergia :: Number -> ActividadDiaria
+disminuirEnergia cantidad (Tripulante n) = Tripulante (max 0 $ n - cantidad)
+
+enfrentarEsqueleto :: ActividadDiaria
+enfrentarEsqueleto tripulante
+    | estaEnLasUltimas tripulante = disminuirEnergia 20 tripulante
+    | otherwise = disminuirEnergia 10 tripulante
+
+
+transportarCarga :: Number -> ActividadDiaria
+transportarCarga peso tripulante = disminuirEnergia peso tripulante
+
+beberGrog :: ActividadDiaria
+beberGrog = beberGrogs 1
+
+beberGrogs :: Number -> ActividadDiaria
+beberGrogs cantidad tripulante = aumentarEnergia (20 * cantidad) tripulante
+
+murio :: Tripulante -> Bool
+murio = (==0) . energia
+
+data Barco = Barco { tripulacion :: [Tripulante], tipoDeBarco :: TipoDeBarco, oro :: Oro, balas :: Number, madera :: Number } deriving (Eq, Show)
+
+data TipoDeBarco = Galeon | Bergatin | Balardo deriving (Show, Eq)
+
+esBarcoFantasma :: Barco -> Bool
+esBarcoFantasma = all murio . tripulacion
+
+
+metrosCuadrados :: Barco -> Number
+metrosCuadrados = tamanioSegunTipoDeBarco . tipoDeBarco
+
+tamanioSegunTipoDeBarco :: TipoDeBarco -> Number
+tamanioSegunTipoDeBarco Galeon = 150
+tamanioSegunTipoDeBarco Bergatin = 100
+tamanioSegunTipoDeBarco Balardo = 50
+
+conOro :: Oro -> Barco -> Barco
+conOro oro (Barco tripulacion tipoDeBarco _ balas madera) =
+    Barco tripulacion tipoDeBarco oro balas madera
+
+aumentarOro :: Oro -> Barco -> Barco
+aumentarOro masOro (Barco tripulacion tipoDeBarco oro balas madera) =
+    Barco tripulacion tipoDeBarco (oro + masOro) balas madera
+
+
+conBalas :: Number -> Barco -> Barco
+conBalas balas (Barco tripulacion tipoDeBarco oro _ madera) =
+    Barco tripulacion tipoDeBarco oro balas madera
+
+aumentarBalas :: Number -> Barco -> Barco
+aumentarBalas masBalas (Barco tripulacion tipoDeBarco oro balas madera) =
+    Barco tripulacion tipoDeBarco oro (balas + masBalas) madera
+
+conMadera :: Number -> Barco -> Barco
+conMadera madera (Barco tripulacion tipoDeBarco oro balas _) =
+    Barco tripulacion tipoDeBarco oro balas madera
+
+aumentarMadera :: Number -> Barco -> Barco
+aumentarMadera masMadera (Barco tripulacion tipoDeBarco oro balas madera) =
+    Barco tripulacion tipoDeBarco oro balas (madera + masMadera)
+
+cantidadTripulantes :: Barco -> Number
+cantidadTripulantes = length . tripulacion
+
+llenarAMaximaCapacidad :: Barco -> Barco
+llenarAMaximaCapacidad barco =
+    (conOro (7 * metrosCuadrados barco) . conBalas (3 * cantidadTripulantes barco)) barco
+
+sumarOro :: Number -> Barco -> Barco
+sumarOro cantidad barco = barco { oro = oro barco + cantidad }
+
+llevarseRecursosDe :: Barco -> Barco -> Barco
+llevarseRecursosDe barco barcoSaqueado =
+    (aumentarMadera (madera barcoSaqueado) .
+    aumentarOro (oro barcoSaqueado) .
+    aumentarBalas (balas barcoSaqueado)) barco
+
+enfrentarse :: Barco -> Barco -> Barco
+enfrentarse barco enemigo
+    | gana barco enemigo = llevarseRecursosDe barco enemigo
+    | otherwise = (conOro 0 . conBalas 0 . conMadera 0) barco
+
+gana :: Barco -> Barco -> Bool
+gana unBarco otroBarco
+    | metrosCuadrados unBarco > metrosCuadrados otroBarco = balas unBarco >= balas otroBarco
+    | metrosCuadrados unBarco == metrosCuadrados otroBarco = madera unBarco >= madera otroBarco
+    | otherwise = cantidadDeTripulantesConVida unBarco >= cantidadDeTripulantesConVida otroBarco
+
+-- Alternativa
+-- tieneMas :: Ord b => (a -> b) -> a -> a -> Bool
+-- tieneMas f x y = f x > f y
+
+-- tieneMasOIgual :: Ord b => (a -> b) -> a -> a -> Bool
+-- tieneMasOIgual f x y = f x >= f y
+
+-- gana :: Barco -> Barco -> Bool
+-- gana unBarco otroBarco
+--     | tieneMas metrosCuadrados unBarco otroBarco = tieneMasOIgual balas unBarco otroBarco
+--     | tieneMas metrosCuadrados otroBarco unBarco = tieneMasOIgual cantidadDeTripulantesConVida unBarco otroBarco
+--     | otherwise  = tieneMasOIgual madera unBarco otroBarco
+
+
+conVida :: Tripulante -> Bool
+conVida = not . murio
+
+cantidadDeTripulantesConVida :: Barco -> Number
+cantidadDeTripulantesConVida = length . tripulantesConVida
+
+tripulantesConVida :: Barco -> [Tripulante]
+tripulantesConVida = filter conVida . tripulacion
+
+conTripulacion :: [Tripulante] -> Barco -> Barco
+conTripulacion nuevaTripulacion (Barco _ tipoDeBarco oro balas madera) =
+    Barco nuevaTripulacion tipoDeBarco oro balas madera
+
+mapTripulantes :: (Tripulante -> Tripulante) -> Barco -> Barco
+mapTripulantes afectarTripulante barco =
+    conTripulacion (map afectarTripulante (tripulacion barco)) barco
+
+cargarEntreQuienesPueden :: Number -> Barco -> Barco
+cargarEntreQuienesPueden peso barco = mapTripulantes (transportarCarga (peso / cantidadDeTripulantesConVida barco)) barco
+
+type Suceso = Barco -> Barco
+
+embarcarTesoro :: Number -> Suceso
+embarcarTesoro pesoEnOro = cargarEntreQuienesPueden pesoEnOro . aumentarOro pesoEnOro
+
+encontrarCargamentoDeGrog :: Suceso
+encontrarCargamentoDeGrog = mapTripulantes (beberGrogs 5)
+
+lucharContraEsqueletos :: Number -> Tripulante -> Tripulante
+lucharContraEsqueletos cantidad tripulante = foldr ($) tripulante (replicate cantidad enfrentarEsqueleto)
+
+conPrimerTripulanteConVida :: (Tripulante -> Tripulante) -> Barco -> Barco
+conPrimerTripulanteConVida afectarTripulante barco =
+    conTripulacion
+        (takeWhile murio (tripulacion barco) ++
+         map afectarTripulante ((take 1 . dropWhile murio . tripulacion) barco) ++
+         (drop 1 . dropWhile murio . tripulacion) barco)
+         barco
+
+conPrimerTripulanteQue :: (Tripulante -> Bool) -> (Tripulante -> Tripulante) -> Barco -> Barco
+conPrimerTripulanteQue condicion afectarTripulante barco =
+    conTripulacion
+        (takeWhile (not.condicion) (tripulacion barco) ++
+         map afectarTripulante ((take 1 . dropWhile (not.condicion) . tripulacion) barco) ++
+         (drop 1 . dropWhile (not.condicion) . tripulacion) barco)
+         barco
+
+enfrentarEsqueletos :: Number -> Suceso
+enfrentarEsqueletos cantidad = conPrimerTripulanteQue (not . murio) (lucharContraEsqueletos cantidad)
+
+precioGrog :: Number
+precioGrog = 30
+
+pagar :: Number -> Barco -> Barco
+pagar cantidad (Barco tripulacion tipoDeBarco oro balas madera) =
+    Barco tripulacion tipoDeBarco (oro - cantidad) balas madera
+
+algunTripulanteMurio :: Barco -> Bool
+algunTripulanteMurio = any murio . tripulacion
+
+pasarPorUnaTiendaDeGrog :: Suceso
+pasarPorUnaTiendaDeGrog barco
+    | oro barco >= precioGrog && algunTripulanteMurio barco =
+        (conPrimerTripulanteQue murio beberGrog . pagar precioGrog) barco
+    | otherwise = barco
+
+suceder :: Suceso -> Barco -> Barco
+suceder suceso barco
+    | esBarcoFantasma barco = barco
+    | otherwise = suceso barco
+
+type Oro = Number
+
+type Recompensa = Barco -> Oro
+
+data Travesia = Travesia { sucesos :: [Suceso], recompensa :: Recompensa }
+
+pasarTodosLosSucesos :: Travesia -> Barco -> Barco
+pasarTodosLosSucesos travesia barco = foldl (flip suceder) barco (sucesos travesia)
+
+cobrarRecompensa :: Travesia -> Barco -> Barco
+cobrarRecompensa travesia barco
+    | esBarcoFantasma barco = barco
+    | otherwise = aumentarOro (recompensa travesia barco) barco
+
+fuerteDeLosCondenados :: Travesia
+fuerteDeLosCondenados = Travesia {
+    sucesos = [enfrentarEsqueletos 100, pasarPorUnaTiendaDeGrog, embarcarTesoro 30],
+    recompensa = (\_ -> 50)
+}
+
+realizarTravesia :: Travesia -> Barco -> Barco
+realizarTravesia travesia barco = (cobrarRecompensa travesia . pasarTodosLosSucesos travesia) barco
+
+travesiaDelFlameheart :: Travesia
+travesiaDelFlameheart = Travesia {
+    sucesos = [
+        flip enfrentarse (Barco {
+            tripulacion = replicate 4 (Tripulante 30),
+            madera = 50,
+            balas = 50,
+            tipoDeBarco = Galeon,
+            oro = 0
+        }),
+        flip enfrentarse (Barco {
+            tripulacion = replicate 3 (Tripulante 10),
+            madera = 30,
+            balas = 30,
+            tipoDeBarco = Bergatin,
+            oro = 0
+        }),
+        encontrarCargamentoDeGrog,
+    embarcarTesoro 150
+ ],
+ recompensa = cantidadDeTripulantesConVida
+}
+
+laGirita :: Travesia
+laGirita = Travesia {
+    sucesos = replicate 4 pasarPorUnaTiendaDeGrog ++ [enfrentarEsqueletos 10],
+    recompensa = oro
+}
--- src/Spec.hs
+++ src/Spec.hs
@@ -4,11 +4,165 @@ import Library
 import Test.Hspec
 import Control.Exception (evaluate)
 
+barcoConTripulacion :: [Tripulante] -> Barco
+barcoConTripulacion tripulacion = unBarco { tripulacion = tripulacion }
+
+barcoDeTipo :: TipoDeBarco -> Barco
+barcoDeTipo tipoDeBarco = unBarco { tipoDeBarco = tipoDeBarco }
+
 correrTests :: IO ()
 correrTests = hspec $ do
-   describe "Parcial" $ do
-     it "Test de ejemplo" $ do
-       2 + 2 `shouldBe` 4
+   describe "tripulantes" $ do
+     describe "esta en las ultimas" $ do
+       it "es verdad si tiene menos de 50 de energia" $ do
+         estaEnLasUltimas (Tripulante 40) `shouldBe` True
+       it "es falso si tiene 0 de energia" $ do
+         estaEnLasUltimas (Tripulante 50) `shouldBe` False
+       it "es falso si tiene mas de 50 de energia" $ do
+         estaEnLasUltimas (Tripulante 51) `shouldBe` False
+     describe "enfrentar un esqueleto" $ do
+       it "le saca 10 de energia a menos que este en las ultimas" $ do
+         enfrentarEsqueleto (Tripulante 100) `shouldBe` Tripulante 90
+       it "le saca 20 de energia si esta en las ultimas" $ do
+         enfrentarEsqueleto (Tripulante 40) `shouldBe` Tripulante 20
+       it "no lo puede dejar en menos de 0 de energia" $ do
+         enfrentarEsqueleto (Tripulante 5) `shouldBe` Tripulante 0
+     describe "transportar una carga" $ do
+       it "le saca tanta energia como la carga transportada" $ do
+         transportarCarga 20 (Tripulante 30) `shouldBe` Tripulante 10
+         transportarCarga 5 (Tripulante 10) `shouldBe` Tripulante 5
+       it "no le puede dejar menos de 0 de energia" $ do
+         transportarCarga 10 (Tripulante 5) `shouldBe` Tripulante 0
+     describe "beber grog" $ do
+       it "le aumenta la energia en 20" $ do
+         beberGrog (Tripulante 0) `shouldBe` Tripulante 20
+         beberGrog (Tripulante 15) `shouldBe` Tripulante 35
+     describe "murio" $ do
+       it "es verdad si tiene 0 de energia" $ do
+         murio (Tripulante 0) `shouldBe` True
+       it "es falso si tiene mas de 0 de energia" $ do
+         murio (Tripulante 5) `shouldBe` False
+   describe "barcos" $ do
+     describe "es barco fantasma" $ do
+       it "es verdad si no tiene tripulacion" $ do
+         esBarcoFantasma (barcoConTripulacion []) `shouldBe` True
+       it "es falso si tiene algun tripulante con vida" $ do
+         esBarcoFantasma (barcoConTripulacion [Tripulante 10, Tripulante 0]) `shouldBe` False
+       it "es verdad si no tiene ningun tripulante con vida" $ do
+         esBarcoFantasma (barcoConTripulacion [Tripulante 0, Tripulante 0]) `shouldBe` True
+     describe "metrosCuadrados" $ do
+       it "los galeones miden 150m2" $ do
+         metrosCuadrados (barcoDeTipo Galeon) `shouldBe` 150
+       it "los bergatines miden 100m2" $ do
+         metrosCuadrados (barcoDeTipo Bergatin) `shouldBe` 100
+       it "los balardos miden 50m2" $ do
+         metrosCuadrados (barcoDeTipo Balardo) `shouldBe` 50
+     describe "llenar a maxima capacidad" $ do
+       it "el barco queda con 7 de oro x metro cuadrado" $ do
+         oro (llenarAMaximaCapacidad (barcoDeTipo Galeon)) `shouldBe` 1050
+         oro (llenarAMaximaCapacidad (barcoDeTipo Bergatin)) `shouldBe` 700
+         oro (llenarAMaximaCapacidad (barcoDeTipo Balardo)) `shouldBe` 350
+       it "el barco queda con 3 balas por persona" $ do
+         balas (llenarAMaximaCapacidad (barcoConTripulacion [Tripulante 0])) `shouldBe` 3
+         balas (llenarAMaximaCapacidad (barcoConTripulacion [Tripulante 0, Tripulante 10])) `shouldBe` 6
+     describe "enfrentamiento" $ do
+       it "tras enfrentarse con un enemigo y perder, se queda sin recursos" $ do
+         enfrentarse (unBarco { tipoDeBarco = Galeon, madera = 1 })
+                     (unBarco { tipoDeBarco = Galeon, madera = 2 })
+                        `shouldBe` (unBarco { tipoDeBarco = Galeon, oro = 0, madera = 0, balas = 0 })
+       it "tras enfrentarse con un enemigo y ganar, se queda con los recursos de su enemigo" $ do
+         enfrentarse (unBarco { tipoDeBarco = Galeon, madera = 2, oro = 1, balas = 2})
+                     (unBarco { tipoDeBarco = Galeon, madera = 1, oro = 15, balas = 5})
+                        `shouldBe` (unBarco { tipoDeBarco = Galeon, madera = 3, oro = 16, balas = 7 })
+     describe "gana" $ do
+       describe "siendo mas grande que el enemigo" $ do
+         it "es verdad cuando tiene igual cantidad de balas" $ do
+           gana ((barcoDeTipo Bergatin) { balas = 1 })
+                ((barcoDeTipo Balardo) { balas = 1 }) `shouldBe` True
+         it "es verdad cuando tiene mas balas" $ do
+           gana ((barcoDeTipo Bergatin) { balas = 2 })
+                ((barcoDeTipo Balardo) { balas = 1 }) `shouldBe` True
+         it "es falso cuando tiene menos balas" $ do
+           gana ((barcoDeTipo Bergatin) { balas = 1 })
+                ((barcoDeTipo Balardo) { balas = 2 }) `shouldBe` False
+     describe "siendo igual de grande que el enemigo" $ do
+       it "es verdad cuando tiene mas madera" $ do
+         gana (unBarco { madera = 2 }) (unBarco { madera = 1 }) `shouldBe` True
+       it "es verdad cuando tienen igual cantidad de madera" $ do
+         gana (unBarco { madera = 1 }) (unBarco { madera = 1 }) `shouldBe` True
+       it "es falso cuando tiene menos madera" $ do
+         gana (unBarco { madera = 1 }) (unBarco { madera = 2 }) `shouldBe` False
+     describe "siendo mas chico que el enemigo" $ do
+       it "es verdad cuando tiene mas tripulantes con vida" $ do
+         gana ((barcoDeTipo Balardo) { tripulacion = [Tripulante 10] })
+             ((barcoDeTipo Bergatin) { tripulacion = [Tripulante 0] })
+             `shouldBe` True
+       it "es verdad cuando tiene igual cantidad de tripulantes con vida" $ do
+          gana ((barcoDeTipo Balardo) { tripulacion = [Tripulante 10] })
+             ((barcoDeTipo Bergatin) { tripulacion = [Tripulante 10] })
+             `shouldBe` True
+       it "es verdad cuando tiene menos tripulantes con vida" $ do
+         gana ((barcoDeTipo Balardo) { tripulacion = [Tripulante 10] })
+             ((barcoDeTipo Bergatin) { tripulacion = [Tripulante 10, Tripulante 15] })
+             `shouldBe` False
+     describe "sucesos" $ do
+       describe "embarcar un tesoro" $ do
+         it "el barco aumenta su oro en el peso del tesoro y sus tripulantes con vida pierden tanta energia como el peso dividido la cantidad de ellos" $ do
+           embarcarTesoro 150 (unBarco { tripulacion = [Tripulante 100, Tripulante 80, Tripulante 0], oro = 5 })
+           `shouldBe` (unBarco { tripulacion = [Tripulante 25, Tripulante 5, Tripulante 0], oro = 155 })
+       describe "encontrar un cargamento de grog" $ do
+         it "toda la tripulacion del barco bebe 5 grog, aumentando su energia" $ do
+           encontrarCargamentoDeGrog (barcoConTripulacion [Tripulante 0, Tripulante 100 ])
+           `shouldBe` barcoConTripulacion [Tripulante 100, Tripulante 200]
+       describe "enfrentar un ejercito de esqueletos" $ do
+         it "si el barco tiene un solo tripulante con vida, le envia a enfrentar a todos los esqueletos uno tras otro" $ do
+           enfrentarEsqueletos 5 (barcoConTripulacion [Tripulante 100])
+           `shouldBe` barcoConTripulacion [Tripulante 50]
+         it "si el barco no tiene tripulantes con vida, queda igual" $ do
+           enfrentarEsqueletos 5 (barcoConTripulacion [])
+           `shouldBe` barcoConTripulacion []
+         it "si el barco tiene varios tripulantes con vida, envia al primero a pelear contra los esqueletos" $ do
+           enfrentarEsqueletos 3 (barcoConTripulacion [Tripulante 60, Tripulante 20])
+           `shouldBe` barcoConTripulacion [Tripulante 20, Tripulante 20]
+       describe "pasarPorUnaTiendaDeGrog" $ do
+         it "si el barco no puede pagar el grog, queda igual" $ do
+           pasarPorUnaTiendaDeGrog (unBarco { oro = 20 }) `shouldBe` unBarco { oro = 20 }
+         it "si puede pagar el grog pero no tiene tripulantes que hayan muerto, queda igual" $ do
+           pasarPorUnaTiendaDeGrog (unBarco { oro = 50, tripulacion = [Tripulante 10] })
+           `shouldBe` unBarco { oro = 50, tripulacion = [Tripulante 10] }
+         it "si puede pagar el grog y tiene algun tripulante que haya muerto, paga el grog y se lo da al primer tripulante muerto" $ do
+           pasarPorUnaTiendaDeGrog (unBarco { oro = 50, tripulacion = [Tripulante 0, Tripulante 10] })
+           `shouldBe` unBarco { oro = 20, tripulacion = [Tripulante 20, Tripulante 10] }
+     it "un suceso nunca le sucede a un barco fantasma" $ do
+      suceder encontrarCargamentoDeGrog (barcoConTripulacion [Tripulante 0]) `shouldBe` barcoConTripulacion [Tripulante 0]
+      suceder pasarPorUnaTiendaDeGrog ((barcoConTripulacion [Tripulante 0]) { oro = 100 }) `shouldBe` (barcoConTripulacion [Tripulante 0]) { oro = 100 }
+     describe "travesia" $ do
+       it "el barco que la logra superar sin convertirse en un barco fantasma cobra la recompensa" $ do
+         realizarTravesia (Travesia [enfrentarEsqueletos 5] (\_ -> 20))
+                         (unBarco { tripulacion = [Tripulante 100], oro = 0 })
+                         `shouldBe` 
+                         (unBarco { tripulacion = [Tripulante 50], oro = 20 })
+     describe "fuerte de los condenados" $ do
+       it "consiste en enfrentar 100 esqueletos pasar por una tienda de grog y embarca 30kg de oro no es fantasma, se cobra 50kg de oro como recompensa" $ do
+         realizarTravesia fuerteDeLosCondenados (unBarco { tripulacion = [Tripulante 2000, Tripulante 0], oro = 30 }) `shouldBe`
+              (unBarco { tripulacion = [Tripulante 985, Tripulante 5], oro = 80 })
+         realizarTravesia fuerteDeLosCondenados (unBarco { tripulacion = [Tripulante 50, Tripulante 15], oro = 0 }) `shouldBe`
+              (unBarco { tripulacion = [Tripulante 0, Tripulante 0], oro = 30 })
+     describe "travesia del flameheart" $ do
+       it "consiste en enfrentar un galeon con 4 tripulantes de 30 energia c/u, 50 tablones de madera y 50 balas de canion, luego un bergatin con 3 tripulantes con 10 de energia c/u y con 30 maderas y 30 balas de canion, luego encontrar un cargamento de grog y embarcar un tesoro de 150kg de oro, y su recompensa es de 200kg de oro por cada tripulante con vida" $ do
+         realizarTravesia travesiaDelFlameheart (unBarco { tripulacion = [Tripulante 1], tipoDeBarco = Galeon, madera = 60, balas = 50, oro = 0 })
+              `shouldBe` unBarco { tripulacion = [Tripulante 0], tipoDeBarco = Galeon, madera = 140, balas = 130, oro = 150 }
+         realizarTravesia travesiaDelFlameheart (unBarco { tripulacion = [Tripulante 200], tipoDeBarco = Galeon, madera = 60, balas = 50, oro = 0 })
+              `shouldBe` unBarco { tripulacion = [Tripulante 150], tipoDeBarco = Galeon, madera = 140, balas = 130, oro = 151 }
+     describe "la girita" $ do
+       it "consiste en pasar 4 veces por una tienda de grog y pelear contra un ejercito de 10 esqueletos, la recompensa es duplicar el oro del barco" $ do
+         realizarTravesia laGirita (unBarco { tripulacion = [Tripulante 200, Tripulante 0, Tripulante 0, Tripulante 0, Tripulante 0], oro = 130 })
+            `shouldBe` unBarco { tripulacion = [Tripulante 100, Tripulante 20, Tripulante 20, Tripulante 20, Tripulante 20], oro = 20 }
+         realizarTravesia laGirita (unBarco { tripulacion = [Tripulante 0], oro = 130 })
+            `shouldBe` unBarco { tripulacion = [Tripulante 0], oro = 130 }
+
+unBarco :: Barco
+unBarco = Barco { tripulacion = [Tripulante 10, Tripulante 20], tipoDeBarco = Galeon, oro = 50, balas = 5, madera = 2 }
 
 escribime :: Expectation
 escribime = implementame
